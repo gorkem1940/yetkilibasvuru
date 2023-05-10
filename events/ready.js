a587a01921476e23fd5410a5131f9f650ced5d44
gorkem <113816295+gorkem1940@users.noreply.github.com>
2023-05-10 11:21:07 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,5 +3,5 @@ const moment = require("moment");
 const Discord = require("discord.js");
 const ayarlar = require('../ayarlar.json');
 module.exports = async client => {
-  client.user.setPresence({ activity: { type: "LISTENING", name: `TheUnbeaten ❤️ Tanrısız `}, status: 'idle' })
+  client.user.setPresence({ activity: { type: "LISTENING", name: `TheUnbeaten ❤️ Görkem `}, status: 'idle' })
 };
